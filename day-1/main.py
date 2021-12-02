be194fbd6eaba128fe42f98fd4b2958fd8176eb2
Aaron Marriner <amarriner@amarriner.com>
2021-12-02 08:27:17 -0500

    Initial commit

---
+++ day-1/main.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def main():
+
+    with open("input.txt") as f:
+        input = f.read().split("\n")
+
+    last = -1
+    increased = 0
+    for line in input:
+        if line == "":
+            continue
+
+        if last == -1:
+            last = line
+            continue
+
+        if last > line:
+            increased += 1
+
+        last = line
+
+    print(f"Depth increased {increased} times")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
